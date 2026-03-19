92d93ad3f3db7cb58618c367bb2834859d6d484d
lirivibe <chriswilliam.crealab@gmail.com>
2026-03-19 08:22:08 +0000

    doc(learned): locators, actions, assertions and architecture POM

---
+++ tests/example.spec.ts
@@ -0,0 +1,28 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
+
+test('Fill a placeholder', async ({ page }) => {
+  await page.goto('https://html.com/')
+
+  // Place the product name inside the search box
+  await page.getByPlaceholder('Search').fill("Laptop");
+
+  // Click the GO button to search for the product
+  await page.getByRole("button", {name: 'Search'}).click();
+});
